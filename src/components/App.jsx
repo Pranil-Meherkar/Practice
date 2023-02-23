d1e8a969cbc1610bce21f70659d1a209eeec96b9
Pranil-Meherkar <pranil@nimapinfotech.com>
2023-02-23 18:05:54 +0530

    added lazy routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,53 @@
 import "../App.css";
-import PrevValueExample from "./PrevValueExample";
+import React, { Suspense } from "react";
 import Homepage from "./Homepage";
-import HOCExample from "./HOCExample";
-import PromiseExample from "./PromiseExample";
+
+// import PrevValueExample from "./PrevValueExample";
+// import HOCExample from "./HOCExample";
+// import PromiseExample from "./PromiseExample";
+// import PureComponentExample from "./PureComponentExample";
 import { Routes, Route } from "react-router-dom";
-import PureComponentExample from "./PureComponentExample";
+const PrevValueExample = React.lazy(() => import("./PrevValueExample"));
+const HOCExample = React.lazy(() => import("./HOCExample"));
+const PromiseExample = React.lazy(() => import("./PromiseExample"));
+const PureComponentExample = React.lazy(() => import("./PureComponentExample"));
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/prev-value-example" element={<PrevValueExample />} />
-        <Route path="/hoc-example" element={<HOCExample />} />
-        <Route path="/promise-example" element={<PromiseExample />} />
+        <Route
+          path="/prev-value-example"
+          element={
+            <Suspense fallback="loading...">
+              <PrevValueExample />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/hoc-example"
+          element={
+            <Suspense fallback="loading...">
+              <HOCExample />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/promise-example"
+          element={
+            <Suspense fallback="loading...">
+              <PromiseExample />
+            </Suspense>
+          }
+        />
         <Route
           path="/pure-component-example"
-          element={<PureComponentExample />}
+          element={
+            <Suspense fallback="loading...">
+              <PureComponentExample />
+            </Suspense>
+          }
         />
       </Routes>
     </>
