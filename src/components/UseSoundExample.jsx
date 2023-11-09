0ca985e25275b13cbc29bd460c9eddc629703798
Pranil-Meherkar <pranil@nimapinfotech.com>
2023-11-09 09:22:19 +0530

    multiplayer added

--- src/components/UseSoundExample.jsx
+++ src/components/UseSoundExample.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import useSound from 'use-sound';
-import kesariya from "../assets/mp3/Kesariya.mp3"
+// import kesariya from "/mp3/Kesariya.mp3"
 import styles from "./UseSoundExample.module.scss"
+import { ReactComponent as Previous } from "../assets/svg/skip-back.svg"
+import { ReactComponent as Next } from "../assets/svg/skip-forward.svg"
+import { ReactComponent as Play } from "../assets/svg/play.svg"
+import { ReactComponent as Pause } from "../assets/svg/pause.svg"
+import { ReactComponent as Stop } from "../assets/svg/square.svg"
 
 const UseSoundExample = () => {
+
+    const nameArray = ["Kesariya.mp3", "Qaafirana.mp3"]
+    const musicPathArray = nameArray.length && nameArray.map(name => "/mp3/" + name)
+
     // const [
     //     play, 
     //     {
@@ -18,9 +27,11 @@ const UseSoundExample = () => {
     const [isPlaying, setIsPlaying] = useState(false);
     const [isPaused, setIsPaused] = useState(false);
     const [playedSeconds, setPlayedSeconds] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [dirty, setDirty] = useState(false);
 
     const [play, { stop, pause, duration, sound }] = useSound(
-        kesariya,
+        musicPathArray[currentIndex],
         { 
             volume: 0.5,
         }
@@ -44,6 +55,20 @@ const UseSoundExample = () => {
         setPlayedSeconds(0)
         stop()
     }
+
+    const playPrev = () => {
+        if(currentIndex > 0) {
+            stopMusic()
+            setCurrentIndex(currentIndex - 1)
+        }
+    }
+
+    const playNext = () => {
+        if(currentIndex < nameArray.length-1) {
+            stopMusic()
+            setCurrentIndex(currentIndex + 1)
+        }
+    }
     
     const convertSecToMin = (timeInSec) => {
         if(!timeInSec && timeInSec !== 0) return "--:--"
@@ -57,6 +82,7 @@ const UseSoundExample = () => {
     const totalTime = Math.round(sound?._duration)
     const widthPerSecond = 100/sound?._duration
 
+    
     useEffect(() => {
         const x = isPlaying && setInterval(() => {
             setPlayedSeconds(prev => {
@@ -69,44 +95,44 @@ const UseSoundExample = () => {
         }, 1000);
         
         return () => {
-            clearInterval(x)
+            x && clearInterval(x)
         }
     }, [isPlaying])
 
-    console.log('widthPercentage', playedSeconds, totalTime)
-    return (  
-        <div>
-            <h1>Use Sound Example</h1>
-            <div className={styles.progressContainer}>
-                <div 
-                    className={`${styles.progressBar} ${isPlaying || isPaused ? styles.isPlaying : ""}`}
-                    style={{ width: `${playedSeconds * widthPerSecond}%`, transition: `width linear 1s` }}
-                ></div>
-                <span className={styles.timeStampContainer}>
-                    <span>{convertSecToMin(playedSeconds)}</span>
-                    <span>{convertSecToMin(totalTime)}</span>
-                </span>
-            </div>
-            {
-                !isPlaying ?
-                <button onClick={playMusic}>Play</button>
-                :
-                <button onClick={pauseMusic}>Pause</button> 
-            }
-            <button onClick={stopMusic}>Stop</button>
+    useEffect(() => {
+        setTimeout(() => {
+            dirty && sound && playMusic()
+            !dirty && sound && setDirty(true)
+        }, 1200)
+    }, [sound])
 
-{/* <button
-      onMouseEnter={() => {
-        setIsHovering(true);
-        play();
-      }}
-      onMouseLeave={() => {
-        setIsHovering(false);
-        stop();
-      }}
-    >
-        Hover over me!
-    </button> */}
+    return (  
+        <div className={styles.background}>
+            <div className={styles.mainContainer}>
+                <p className={styles.songName}>{nameArray[currentIndex]}</p>
+                <div className={styles.progressContainer}>
+                    <span className={styles.backdrop}></span>
+                    <div 
+                        className={`${styles.progressBar} ${isPlaying || isPaused ? styles.isPlaying : ""}`}
+                        style={{ width: `${playedSeconds * widthPerSecond}%`, transition: `width linear 1s` }}
+                    ></div>
+                    <span className={styles.timeStampContainer}>
+                        <span>{convertSecToMin(playedSeconds)}</span>
+                        <span>{convertSecToMin(totalTime)}</span>
+                    </span>
+                </div>
+                <div className={styles.buttonContainer}>
+                    <button onClick={playPrev}><Previous className={styles.buttonIcon}/></button>
+                    {
+                        !isPlaying ?
+                        <button onClick={playMusic} disabled={!sound}><Play className={styles.buttonIcon}/></button>
+                        :
+                        <button onClick={pauseMusic}><Pause className={styles.buttonIcon}/></button> 
+                    }
+                    <button onClick={stopMusic}><Stop className={styles.buttonIcon}/></button>
+                    <button onClick={playNext}><Next className={styles.buttonIcon}/></button>
+                </div>
+            </div>
         </div>
     );
 }
